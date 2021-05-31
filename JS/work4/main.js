24cc0fcbb816cd101923ca7b0257a5e89adf2448
Kolterg <mark.reta99@gmail.com>
2021-05-31 15:45:03 +0300

    Completed the fourth work of JS.

--- JS/work4/main.js
+++ JS/work4/main.js
@@ -143,26 +143,73 @@ newEl(type, text);
 // Приймає масив автомобілів (можна взяти з попередніх дз),
 // та індентифікатор елемнту в який потрібно додати список цих автомобілів.
 // Для кожного автомобіля створити свій блок, та додати його в елемент,
-// індентифікатор якого ви отримали. Всі властивості авто в одному блоці. (Які властивості???)
+// індентифікатор якого ви отримали. Всі властивості авто в одному блоці.
 
-let cars = ['Toyota', 'Nisan', 'RollsRoys', 'Hunday', 'Pegot', 'Chevrolet', 'Ford', 'Ferrary'];
+let cars = [
+    {producer:"Subaru",model: "WRX",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400},
+    {producer:"Subaru",model: "Legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250},
+    {producer:"Subaru",model: "Tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300},
+    {producer:"Subaru",model: "Leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140},
+    {producer:"Subaru",model: "Impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200},
+    {producer:"Subaru",model: "Outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165},
+    {producer:"BMW",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120},
+    {producer:"BMW",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120},
+    {producer:"BMW",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350},
+    {producer:"BMW",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180},
+    {producer:"Mercedes",model: "E200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180},
+    {producer:"Mercedes",model: "E63",year: 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400},
+    {producer:"Mercedes",model: "C250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230}
+];
 let id = document.body;
 
 function blockOfCars(cars, id) {
     for (let j = 0; j < cars.length; j++) {
         const car = document.createElement('div');
-        car.innerHTML = `${cars[j]}`;
+        car.innerHTML = `${cars[j].producer} - ${cars[j].model} - ${cars[j].year}`;
         id.appendChild(car);
     }
 }
 
 blockOfCars(cars, id);
 
-// 4 ???
-// Приймає масив автомобілів (можна взяти з попередніх дз ),
+// 4
+// Приймає масив автомобілів (можна взяти з попередніх дз),
 // та індентифікатор елемнту в який потрібно додати список цих автомобілів.
 // Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
 // Для кожної властивості створити всередені блока автомоблія свій блок.
 
+function blockOfCars2(cars, id) {
+    for (let j = 0; j < cars.length; j++) {
+        const car = document.createElement('div');
+        id.appendChild(car);
+        const producer = document.createElement('h3');
+        producer.innerHTML = `${cars[j].producer}`;
+        car.appendChild(producer);
+        const model = document.createElement('div');
+        model.innerHTML = `${cars[j].model}`;
+        car.appendChild(model);
+        const year = document.createElement('div');
+        year.innerHTML = `${cars[j].year}`;
+        car.appendChild(year);
+        const color = document.createElement('div');
+        color.innerHTML = `${cars[j].color}`;
+        car.appendChild(color);
+        const type = document.createElement('div');
+        type.innerHTML = `${cars[j].type}`;
+        car.appendChild(type);
+        const engine = document.createElement('div');
+        engine.innerHTML = `${cars[j].engine}`;
+        car.appendChild(engine);
+        const volume = document.createElement('div');
+        volume.innerHTML = `${cars[j].volume}`;
+        car.appendChild(volume);
+        const power = document.createElement('div');
+        power.innerHTML = `${cars[j].power}`;
+        car.appendChild(power);
+        const hr = document.createElement('div');
+        hr.innerHTML = `<hr/>`;
+        car.appendChild(hr);
+    }
+}
 
-
+blockOfCars2(cars, id);
